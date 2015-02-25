0e49a6a4791cda7e726860093acf9c597cdd3ab1
Volodymyr Svydenko <vladimir.svydenko@keystonett.com>
2015-02-25 16:24:48 +0200

    * array init change way [removing const as size]; * change way to set array with default values; * optimize main part of alg - checking of items is not zero now performs once for all three coins

--- src/com/blogspot/vsvydenko/FindCoinsMaxCount.java
+++ src/com/blogspot/vsvydenko/FindCoinsMaxCount.java
@@ -10,19 +10,17 @@ public class FindCoinsMaxCount {
 
     public static void main(String[] args) {
         int n = 17;
-        int[] m = new int[18];
+        int[] m = new int[n+1];
 
         int a = 2;
         int b = 3;
         int c = 5;
 
         for (int i=1; i<n; i++) {
-            if (i == a || i == b || i == c) {
-                m[i] = 1;
-            } else {
-                m[i] = 0;
-            }
+            m[i] = 0;
         }
+        m[a] = m[b] = m[c] = 1;
+
         //showArrayFor(m);
 
         if (n % a == 0) {
@@ -31,20 +29,22 @@ public class FindCoinsMaxCount {
         }
 
         for (int i = a; i < m.length; i++) {
-            //System.out.println("i = " + i);
-            if (m[i] != 0 && (i + a) <= n && m[i + a] < m[i] + 1 ) {
-                //System.out.println("a passed, increase m[" + (i + a) +"], now it is " + (m[i] + 1));
-                m[i + a] = m[i] + 1;
+            if (m[i] != 0) {
+                //System.out.println("i = " + i);
+                if (i + a <= n && m[i + a] < m[i] + 1 ) {
+                    //System.out.println("a passed, increase m[" + (i + a) +"], now it is " + (m[i] + 1));
+                    m[i + a] = m[i] + 1;
+                }
+                if (i + b <= n && m[i + b] < m[i] + 1) {
+                    //System.out.println("b passed, increase m[" + (i + b) +"], now it is " + (m[i] + 1));
+                    m[i + b] = m[i] + 1;
+                }
+                if (i + c <= n && m[i + c] < m[i] + 1) {
+                    //System.out.println("c passed, increase m[" + (i + c) +"], now it is " + (m[i] + 1));
+                    m[i + c] = m[i] + 1;
+                }
+                //showArrayFor(m);
             }
-            if (m[i] != 0 && (i + b) <= n && m[i + b] < m[i] + 1) {
-                //System.out.println("b passed, increase m[" + (i + b) +"], now it is " + (m[i] + 1));
-                m[i + b] = m[i] + 1;
-            }
-            if (m[i] != 0 && (i + c) <= n && m[i + c] < m[i] + 1) {
-                //System.out.println("c passed, increase m[" + (i + c) +"], now it is " + (m[i] + 1));
-                m[i + c] = m[i] + 1;
-            }
-            //showArrayFor(m);
         }
         System.out.println("Result: " + m[n]);
     }
